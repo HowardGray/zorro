c8f06b7acfb03b6f93d211315e5ca699da05c531
henry2992 <henry2992@hotmail.com>
2018-01-11 08:56:36 -0500

    Add logo and validations for urls

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ActiveRecord::Base
   validates :title, :company, :applyurl, presence: true
+  validates :applyurl, :companyurl, :format => URI::regexp(%w(http https))
   enum level: ['Junior', 'Semi-Senior', 'Senior']
   enum jobtype: ['Education', 'Programming', 'Construction']
 end
